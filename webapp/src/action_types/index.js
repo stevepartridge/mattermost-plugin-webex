587dcaf0f3d42827a482995ee7aefb1a9468d94b
Steve Partridge <srpartridge@gmail.com>
2019-03-25 13:57:08 -0700

    Format updates for eslint

--- webapp/src/action_types/index.js
+++ webapp/src/action_types/index.js
@@ -4,16 +4,16 @@
 import {id as pluginId} from '../manifest';
 
 export default {
-    AUTH_CONNECTED        : pluginId + '_auth_connected',
-    AUTH_DISCONNECTED     : pluginId + '_auth_disconnected',
-    AUTH_REQUIRED         : pluginId + '_auth_required',
-    
-    MEETING_CREATED       : pluginId + '_meeting_created',
-    MEETING_CREATED_ERROR : pluginId + '_meeting_created_error',
-    MEETING_STARTED       : pluginId + '_meeting_started',
-    MEETING_ENDED         : pluginId + '_meeting_ended',
-    
-    STATUS_CHANGE         : pluginId + '_status_change',
-    OPEN_ROOT_MODAL       : pluginId + '_open_root_modal',
-    CLOSE_ROOT_MODAL      : pluginId + '_close_root_modal',
+    AUTH_CONNECTED: pluginId + '_auth_connected',
+    AUTH_DISCONNECTED: pluginId + '_auth_disconnected',
+    AUTH_REQUIRED: pluginId + '_auth_required',
+
+    MEETING_CREATED: pluginId + '_meeting_created',
+    MEETING_CREATED_ERROR: pluginId + '_meeting_created_error',
+    MEETING_STARTED: pluginId + '_meeting_started',
+    MEETING_ENDED: pluginId + '_meeting_ended',
+
+    STATUS_CHANGE: pluginId + '_status_change',
+    OPEN_ROOT_MODAL: pluginId + '_open_root_modal',
+    CLOSE_ROOT_MODAL: pluginId + '_close_root_modal',
 };
--- webapp/src/actions/index.js
+++ webapp/src/actions/index.js
@@ -6,145 +6,140 @@ import {PostTypes} from 'mattermost-redux/action_types';
 import Client from '../client';
 import ActionTypes from '../action_types';
 
-
 export const openRootModal = () => (dispatch) => {
-  dispatch({
-    type: ActionTypes.OPEN_ROOT_MODAL,
-  });
+    dispatch({
+        type: ActionTypes.OPEN_ROOT_MODAL,
+    });
 };
 
 export const closeRootModal = () => (dispatch) => {
-  dispatch({
-    type: ActionTypes.CLOSE_ROOT_MODAL,
-  });
+    dispatch({
+        type: ActionTypes.CLOSE_ROOT_MODAL,
+    });
 };
 
 export function unauthorized() {
-  return (dispatch) => {
-    dispatch({
-      type: ActionTypes.AUTH_DISCONNECTED,
-    });
-  }
-};
+    return (dispatch) => {
+        dispatch({
+            type: ActionTypes.AUTH_DISCONNECTED,
+        });
+    };
+}
 
 export function getConnected() {
-  return (dispatch) => {
+    return (dispatch) => {
+        return Client.getConnected().then((response) => {
+            if (!response) {
+                dispatch({
+                    type: ActionTypes.AUTH_DISCONNECTED,
+                });
+                return;
+            }
+            dispatch({
+                type: ActionTypes.AUTH_CONNECTED,
+                data: response,
+            });
 
-    return Client.getConnected().then(response => {
-
-      if (!response) {
-        dispatch({
-          type: ActionTypes.AUTH_DISCONNECTED
+            return {response};
+        }).catch((error) => {
+            throw (error);
         });
-        return
-      }
-      dispatch({
-        type: ActionTypes.AUTH_CONNECTED,
-        data: response
-      });
-
-      return {response};
-
-    }).catch(error => {
-      throw(error);
-    });
-  }
+    };
 }
 
 export function startMeeting(channelId) {
-  return async (dispatch, getState) => {
-    console.log("start meeting");
+    return async (dispatch, getState) => {
+        console.log('start meeting');
 
-    let data;
-    try {
-      data = await Client.startMeeting(channelId, true);
-    } catch (error) {
-      return {error};
-    }
+        let data;
+        try {
+            data = await Client.startMeeting(channelId, true);
+        } catch (error) {
+            return {error};
+        }
 
-    console.log('data', data)
+        console.log('data', data);
 
-    if (data.error) {
-      dispatch({
-        type: ActionTypes.MEETING_CREATED_ERROR,
-        data,
-      });
+        if (data.error) {
+            dispatch({
+                type: ActionTypes.MEETING_CREATED_ERROR,
+                data,
+            });
 
-      const post = {
-        id: 'webexPlugin' + Date.now(),
-        create_at: Date.now(),
-        update_at: 0,
-        edit_at: 0,
-        delete_at: 0,
-        is_pinned: false,
-        user_id: getState().entities.users.currentUserId,
-        channel_id: channelId,
-        root_id: '',
-        parent_id: '',
-        original_id: '',
-        message: data.message,
-        type: 'system_ephemeral',
-        props: {},
-        hashtags: '',
-        pending_post_id: '',
-      };
+            const post = {
+                id: 'webexPlugin' + Date.now(),
+                create_at: Date.now(),
+                update_at: 0,
+                edit_at: 0,
+                delete_at: 0,
+                is_pinned: false,
+                user_id: getState().entities.users.currentUserId,
+                channel_id: channelId,
+                root_id: '',
+                parent_id: '',
+                original_id: '',
+                message: data.message,
+                type: 'system_ephemeral',
+                props: {},
+                hashtags: '',
+                pending_post_id: '',
+            };
 
-      dispatch({
-        type: PostTypes.RECEIVED_POSTS,
-        data: {
-          order: [],
-          posts: {
-            [post.id]: post,
-          },
-        },
-        channelId,
-      });
+            dispatch({
+                type: PostTypes.RECEIVED_POSTS,
+                data: {
+                    order: [],
+                    posts: {
+                        [post.id]: post,
+                    },
+                },
+                channelId,
+            });
 
-      return data;
-    }
+            return data;
+        }
 
-    dispatch({
-      type: ActionTypes.MEETING_CREATED,
-      data,
-    });
+        dispatch({
+            type: ActionTypes.MEETING_CREATED,
+            data,
+        });
 
-    return {data: data};
-  };
+        return {data};
+    };
 }
 
 export function notifyCanStartMeeting() {
-  return (dispatch, getState) => {
+    return (dispatch, getState) => {
+        const channelId = getState().entities.channels.currentChannelId;
 
-    let channelId = getState().entities.channels.currentChannelId
+        const post = {
+            id: 'webexPlugin' + Date.now(),
+            create_at: Date.now(),
+            update_at: 0,
+            edit_at: 0,
+            delete_at: 0,
+            is_pinned: false,
+            user_id: getState().entities.users.currentUserId,
+            channel_id: channelId,
+            root_id: '',
+            parent_id: '',
+            original_id: '',
+            message: 'Webex account connected.  You can now start a meeting.',
+            type: 'system_ephemeral',
+            props: {},
+            hashtags: '',
+            pending_post_id: '',
+        };
 
-    const post = {
-        id: 'webexPlugin' + Date.now(),
-        create_at: Date.now(),
-        update_at: 0,
-        edit_at: 0,
-        delete_at: 0,
-        is_pinned: false,
-        user_id: getState().entities.users.currentUserId,
-        channel_id: channelId,
-        root_id: '',
-        parent_id: '',
-        original_id: '',
-        message: 'Webex account connected.  You can now start a meeting.',
-        type: 'system_ephemeral',
-        props: {},
-        hashtags: '',
-        pending_post_id: '',
-      };
-
-      dispatch({
-        type: PostTypes.RECEIVED_POSTS,
-        data: {
-          order: [],
-          posts: {
-            [post.id]: post,
-          },
-        },
-        channelId,
-      });
-  }
+        dispatch({
+            type: PostTypes.RECEIVED_POSTS,
+            data: {
+                order: [],
+                posts: {
+                    [post.id]: post,
+                },
+            },
+            channelId,
+        });
+    };
 }
